bf7f4ef9dd844830fed26052068ff4450eb93b1b
mohamed alaa <mohalaa1520@gmail.com>
2023-09-12 22:00:23 +0300

    lastUploading

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-
-import { createBrowserRouter, createHashRouter, RouterProvider } from 'react-router-dom';
-import './App.css';
+import {
+  createBrowserRouter,
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./App.css";
 // import Footer from './components/Footer/Footer';
-import About from './components/About/About';
-import Contact from './components/Contact/Contact';
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
 // import Navbar from './components/Navbar/Navbar';
 import Home from "./components/Home/Home";
-import Notfound from './components/NotFound/Notfound';
-import Layout from './components/Layout/Layout';
-import Portfolio from './components/Portfolio/Portfolio';
+import Notfound from "./components/NotFound/Notfound";
+import Layout from "./components/Layout/Layout";
+import Portfolio from "./components/Portfolio/Portfolio";
 
-
- const router = createHashRouter([
-
-   { path: "", element: <Layout /> , children : [
-    { path: "home", element: <Home /> },
-    { path: "About", element: <About /> },
-    { path: "Contact", element: <Contact /> },
-    { path: "Portfolio", element: <Portfolio /> },
-    { path: "*", element: <Notfound/> },
-  ]},
-   
-   
- ]);
+const router = createHashRouter([
+  {
+    path: "",
+    element: <Layout />,
+    children: [
+      { path: "", element: <Home /> },
+      { path: "home", element: <Home /> },
+      { path: "About", element: <About /> },
+      { path: "Contact", element: <Contact /> },
+      { path: "Portfolio", element: <Portfolio /> },
+      { path: "*", element: <Notfound /> },
+    ],
+  },
+]);
 
 function App() {
   return (
